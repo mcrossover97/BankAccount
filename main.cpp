889f5f0852405d7c019c8b6f817d10e1716a799e
Mohammad Khojastehmehr <mcrossover97@gmail.com>
2021-03-24 03:20:37 +0430

    some early stuff

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "BankAccount.h"
+
+int main() {
+	double loanAmount = 1000;
+	BankOneAccount MohammadBankAccount;
+	MohammadBankAccount.setName("Mohammad");
+	MohammadBankAccount.setAge(23);
+	MohammadBankAccount.setSex("Male");
+	MohammadBankAccount.setPunctualityGrade("A");
+	MohammadBankAccount.setBalance(1000);
+	MohammadBankAccount.setDept(0);
+	MohammadBankAccount.depo(5);
+	loanFunction MohammadLoan = MohammadBankAccount.makeLoanFunction();
+	MohammadLoan(loanAmount);
+	MohammadBankAccount.printName();
+	MohammadBankAccount.printBalance();
+	MohammadBankAccount.printDept();
+
+}
