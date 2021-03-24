25471ee23d99b98d1cf7af5615b57892ca032845
Mohammad Khojastehmehr <mcrossover97@gmail.com>
2021-03-25 01:35:20 +0430

    as of 25 march

--- main.cpp
+++ main.cpp
@@ -7,16 +7,13 @@ int main() {
 	MohammadBankAccount.setName("Mohammad");
 	MohammadBankAccount.setAge(23);
 	MohammadBankAccount.setSex("Male");
-	double d = 30;
 	MohammadBankAccount.setPunctualityGrade("A");
 	MohammadBankAccount.setBalance(1000);
 	MohammadBankAccount.setDept(0);
-	double c = 30;
 	MohammadBankAccount.depo(5);
 	loanFunction MohammadLoan = MohammadBankAccount.makeLoanFunction();
 	MohammadLoan(loanAmount);
 	MohammadBankAccount.printName();
 	MohammadBankAccount.printBalance();
 	MohammadBankAccount.printDept();
-	std::cout << "Goodbye, World!" <<std::endl;
 }
